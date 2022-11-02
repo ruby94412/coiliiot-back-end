5012f6ef9e09e0cb4b297c1f806472b52e97fa6a
ruby94412 <ruby94412@gmail.com>
2022-11-02 16:48:29 +0800

    MBD-21 coiliiot-back-end test support

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const {Router} = require('express');
 const loginRouter = require('./Login');
 const deviceRouter = require('./Device');
 const groupRouter = require('./Group');
+const testRouter = require('./Test');
 
 const router = Router();
 router.use('/login', loginRouter);
 router.use('/device', deviceRouter);
 router.use('/group', groupRouter);
+router.use('/testAir724', testRouter);
+
 module.exports = router;
