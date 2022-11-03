7652e1737fdc1ac860467cf224603aa7ef986148
ruby94412 <ruby94412@gmail.com>
2022-11-03 17:48:21 +0800

    MBD-21 coiliiot-back-end test support

--- routes/Device.js
+++ routes/Device.js
@@ -1,13 +1,6 @@
 const {Router} = require('express');
 const deviceRouter = Router();
-const TEST_JSON_STRING = `{"fota": 0, "uartReadTime": 25, "flow": "", "param_ver": 1, "pw
-rmod": "normal", "password": "", "netReadTime": 0, "passon": 1, "nolog": "1", "plate": 0, "reg": 
-{"type": 3, "data": "273L62XH9LA89ARF", "prefix": "", "postfix": ""}, "convert": 0, "uconf": [[1,
-   "9600", 8, 2, 0, ""], [], []], "conf": [["tcp", {"type": 0, "data": "273L62XH9LA89ARF", "prefix": "
-   ", "postfix": ""}, 300, "mbrtu.tlink.io", "8651", 1, "", "", "", ""], [], [], [], [], [], []], "pre
-   set": {"number": "", "delay": "", "smsword": ""}, "apn": ["", "", ""], "cmds": [[""], [], []], "p
-   ins": ["pio0", "pio0", "pio0"], "gps": {"pio": [], "fun": []}, "upprot": ["", "", "", "", "", "", ""], "dwp
-   rot": ["", "", "", "", "", "", ""], "warn": {"adc0": [], "adc1": [], "vbatt": [], "gpio": []}}`
+const TEST_JSON_STRING = `{"test": "test"}`
 const {
   addDevice,
   deleteDevice,
