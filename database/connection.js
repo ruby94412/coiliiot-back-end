4680b4954aa2a682d723bb5f0d46516b6ae9a00d
ruby94412 <ruby94412@gmail.com>
2022-11-04 15:57:31 +0800

    MBD-19 aliyun database setup test

--- database/connection.js
+++ database/connection.js
@@ -6,40 +6,38 @@ const host2 = "dds-bp1ed7391440f6e42.mongodb.rds.aliyuncs.com";
 const port2 = 3717;
 const username = "root";
 const password = "Zhanglai26"
-const replSetName = "mgset-63015559";
+const replSetName = "mgset-63015559"
 const demoDb = "test";
 const demoColl = "testColl";
-//官方建议使用副本集地址，确保高可用。
+// 官方建议使用副本集地址，确保高可用。
 const url = sprintf("mongodb://%s:%s@%s:%d,%s:%d/admin?replicaSet=%s",
         username, password, host1, port1, host2, port2, replSetName);
 console.info("url:", url);
 const client = new MongoClient(url);
-//获取mongoClient。
-async function connectToDB() {
+// 获取mongoClient。
+async function run() {
     try {
-        //连接实例。
+        // 连接实例。
         await client.connect();
-        //取得数据库句柄。
+        // 取得数据库句柄。
         const database = client.db(demoDb);
-        //取得Collection句柄。
+        // 取得Collection句柄。
         const collection = database.collection(demoColl);
         const demoName = "Node For Demo";
         const doc = { "DEMO": demoName, "MESG": "Hello AliCoudDB For MongoDB" };
         console.info("ready insert document: ", doc);
-        //插入数据。
+        // 插入数据。
         const result = await collection.insertOne(doc);
         console.log(
             `A document was inserted with the _id: ${result.insertedId}`,
         );
-        //读取数据。
+        // 读取数据。
         const filter = { "DEMO": demoName };
         const findResult = await collection.find(filter);
         await findResult.forEach(console.dir);
       } finally {
-          //关闭连接。
+          // 关闭连接。
           await client.close();
       }
 }
-module.exports = {
-  connectToDB,
-}
+run().catch(console.dir);
