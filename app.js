1f721eb843105a26d05828a7864889e80ae54359
ruby94412 <ruby94412@gmail.com>
2023-02-25 10:51:02 +0800

    update app

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const https = require('https');
-const fs = require('fs');
 const app = express();
-const PORT = 8443;
+const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
@@ -13,13 +11,7 @@ require('./routes/Login')(app);
 require('./routes/Group')(app);
 require('./routes/Device')(app);
 
-
-const server = https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app);
-
-server.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
 });
 
